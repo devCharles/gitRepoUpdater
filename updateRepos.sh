9ee443269b74b140c2135807ec96d264628d859f
charles silva <csilva@creze.com>
2017-06-12 21:12:43 -0500

    color added

--- updateRepos.sh
+++ updateRepos.sh
@@ -1,60 +1,80 @@
+#!/bin/bash
+
+# function to print in color
+cecho() {
+  local code="\033["
+  case "$1" in
+    black  | bk) color="${code}0;30m";;
+    red    |  r) color="${code}1;31m";;
+    green  |  g) color="${code}1;32m";;
+    yellow |  y) color="${code}1;33m";;
+    blue   |  b) color="${code}1;34m";;
+    purple |  p) color="${code}1;35m";;
+    cyan   |  c) color="${code}1;36m";;
+    gray   | gr) color="${code}0;37m";;
+    *) local text="$1"
+  esac
+  [ -z "$text" ] && local text="$color$2${code}0m"
+  echo "$text"
+}
+
 current=${PWD}
 yes="yes";
 no="no";
 clear
-echo "---- GIT UPDATER ----"
+echo
+cecho g "\n\t\t\t\t\t---- GIT UPDATER ----\n"
 echo "Do you want to update your repos in folder  $current ? (yes/no)";
-read resp;
+read -p ">>> " resp
 while [ "$resp" != "$yes" ] && [ "$resp" != "$no" ]; do
   echo "please write 'yes' or 'no'"
-  read resp
+  read -p ">>> " resp
 done
 if [ "$resp" == "$yes" ]; then
   clear
   echo "Do you want to update all repositories or want to decide per folder?"
   echo "1) update all"
   echo "2) ask me for each one"
-  read op;
+  read -p ">>> " op;
   opone="1";
   optwo="2"
   while [ "$op" != "$opone" ] && [ "$op" != "$optwo" ]; do
     echo "please choose a valid option"
-    read op
+    read -p ">>> " op;
   done
   if [ "$op" == "$opone" ]; then
     for d in */ ; do
-      echo "---------------------------------------";
-      echo ">>>>>>>>>>>>>>> UPDATING => $d ";
-      echo "---------------------------------------";
+      cecho b "---------------------------------------";
+      cecho b ">>>>>>>>>>>>>>> UPDATING => $d ";
+      cecho b "---------------------------------------";
       cd $d ;
-      echo pwd;
       git fetch | echo ;
       git checkout develop | echo ;
       git pull | echo ;
-      echo "<<<< UPDATED => $d "
+      cecho g "<<<< UPDATED => $d "
       cd .. ;
     done
-    echo "########################################";
-    echo "············ FINISHED ··················";
-    echo "########################################";
+    cecho g "########################################";
+    cecho g "············ FINISHED ··················";
+    cecho g "########################################";
+    cecho g "\nSEE YOU LATER BRO..."
   elif [ "$op" == "$optwo" ];then
     for d in */ ; do
-      echo "Do you want to update (yes/no)=> $d"
-      read res;
+      echo "Do you want to update => $d  (yes/no)"
+      read -p ">>> " res
       while [ "$res" != "$yes" ] && [ "$res" != "$no" ]; do
         echo "please write 'yes' or 'no'"
-        read res
+        read -p ">>> " res
       done
       if [ "$res" == "$yes" ]; then
-        echo "---------------------------------------";
-        echo ">>>>>>>>>>>>>>> UPDATING => $d ";
-        echo "---------------------------------------";
+        cecho b "---------------------------------------";
+        cecho b ">>>>>>>>>>>>>>> UPDATING => $d ";
+        cecho b "---------------------------------------";
         cd $d ;
-        echo pwd;
         git fetch | echo ;
-        git checkout develop | echo ;
-        git pull | echo ;
-        echo "<<<< UPDATED => $d "
+        git checkout develop | echo  ;
+        git pull | echo  ;
+        cecho g "<<<< UPDATED => $d "
         cd .. ;
       else
         echo "$d Was not updated"
