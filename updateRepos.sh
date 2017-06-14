0e2575c20ca0e13c8ddbd087a9c0fe0fba680c99
Carlos Silva <charlesipn@gmail.com>
2017-06-13 22:16:36 -0500

    update function added

--- updateRepos.sh
+++ updateRepos.sh
@@ -1,35 +1,40 @@
 #!/bin/bash
 
-# function to print in color
-cecho() {
-  local code="\033["
-  case "$1" in
-    black  | bk) color="${code}0;30m";;
-    red    |  r) color="${code}1;31m";;
-    green  |  g) color="${code}1;32m";;
-    yellow |  y) color="${code}1;33m";;
-    blue   |  b) color="${code}1;34m";;
-    purple |  p) color="${code}1;35m";;
-    cyan   |  c) color="${code}1;36m";;
-    gray   | gr) color="${code}0;37m";;
-    *) local text="$1"
-  esac
-  [ -z "$text" ] && local text="$color$2${code}0m"
-  echo "$text"
+update(){
+  dir=$1;
+  echo "---------------------------------------";
+  echo ">>>>>>>>>>>>>>> UPDATING => $dir ";
+  echo "---------------------------------------";
+  cd $dir ;
+  echo ${PWD}
+  git fetch | echo ;
+  git checkout develop | echo ;
+  git pull | echo ;
+  echo "<<<< UPDATED => $dir "
+  cd .. ;
 }
 
 current=${PWD}
 yes="yes";
 no="no";
 clear
-echo
-cecho g "\n\t\t\t\t\t---- GIT UPDATER ----\n"
+
+if [ "$1" == "-a" ] || [ "$1" == "all" ]; then
+  echo "all :D"
+elif [ "$1" == "-e" ] || [ "$1" == "each" ]; then
+  echo "each :D"
+else
+  echo "ninguno :("
+fi
+
+echo "---- GIT UPDATER ----"
 echo "Do you want to update your repos in folder  $current ? (yes/no)";
 read -p ">>> " resp
 while [ "$resp" != "$yes" ] && [ "$resp" != "$no" ]; do
   echo "please write 'yes' or 'no'"
   read -p ">>> " resp
 done
+
 if [ "$resp" == "$yes" ]; then
   clear
   echo "Do you want to update all repositories or want to decide per folder?"
@@ -44,20 +49,12 @@ if [ "$resp" == "$yes" ]; then
   done
   if [ "$op" == "$opone" ]; then
     for d in */ ; do
-      cecho b "---------------------------------------";
-      cecho b ">>>>>>>>>>>>>>> UPDATING => $d ";
-      cecho b "---------------------------------------";
-      cd $d ;
-      git fetch | echo ;
-      git checkout develop | echo ;
-      git pull | echo ;
-      cecho g "<<<< UPDATED => $d "
-      cd .. ;
+      update $d;
     done
-    cecho g "########################################";
-    cecho g "············ FINISHED ··················";
-    cecho g "########################################";
-    cecho g "\nSEE YOU LATER BRO..."
+    echo "########################################";
+    echo "············ FINISHED ··················";
+    echo "########################################";
+    echo "\nSEE YOU LATER BRO..."
   elif [ "$op" == "$optwo" ];then
     for d in */ ; do
       echo "Do you want to update => $d  (yes/no)"
@@ -67,15 +64,7 @@ if [ "$resp" == "$yes" ]; then
         read -p ">>> " res
       done
       if [ "$res" == "$yes" ]; then
-        cecho b "---------------------------------------";
-        cecho b ">>>>>>>>>>>>>>> UPDATING => $d ";
-        cecho b "---------------------------------------";
-        cd $d ;
-        git fetch | echo ;
-        git checkout develop | echo  ;
-        git pull | echo  ;
-        cecho g "<<<< UPDATED => $d "
-        cd .. ;
+        update $d;
       else
         echo "$d Was not updated"
       fi
